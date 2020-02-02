832d4c9f617e38dbc7487a82f1abbce106d092b9
Alex Samland <alexandersamland2020@u.northwestern.edu>
2020-02-01 20:32:27 -0600

    finished camera class

--- src/camera.py
+++ src/camera.py
@@ -3,15 +3,20 @@
 # Created Feb 1, 2020
 # Alex Samland (alexsamland@u.northwestern.edu)
 
+import cv2
+import numpy as np
+
 class Camera(object):
 
 
-    def __init__(self, video_source=0, save_video=None, frame_width, frame_height,\
-        fps, autofocus=0 ,manual_focus=0, brightness=30, contrast=100):
+    def __init__(self, frame_width, frame_height, fps, video_source=0, save_video=None,\
+        show_video=True, roi_dims=None, autofocus=0 ,focus_level=0, brightness=30, contrast=100):
         '''Initializes camera with specified settings as tuned tracking settings
         (ie. turns off autofocus, sets brightness and contrast)'''
         # Camera Settings
         self.video_source = video_source
+        self.save_video = save_video
+        self.show_video = show_video
         self.autofocus = autofocus
         self.focus_level = focus_level
         self.brightness = brightness
@@ -21,9 +26,16 @@ class Camera(object):
         self.cap.set(6, self.fourcc) # setting MJPG codec
         self.cap.set(3, frame_width) # Width
         self.cap.set(4, frame_height) # Height
-        self.frame_width = self.cap.get(3)
-        self.frame_height = self.cap.get(4)
-        self.cap.set(5,self.fps) # fps
+        self.frame_width = int(self.cap.get(3))
+        self.frame_height = int(self.cap.get(4))
+
+        if roi_dims is None:
+            self.roi_dims = [0,0,self.frame_height,self.frame_width]
+        else:
+            assert len(roi_dims) is 4, 'roi_dims is 4 element list of form: [x, y, w, h]'
+            self.roi_dims = roi_dims
+
+        self.cap.set(5,fps) # fps
         self.fps = self.cap.get(5)
         self.cap.set(cv2.CAP_PROP_AUTOFOCUS, self.autofocus) # disable autofocus
         if self.focus_level is not None:
@@ -35,6 +47,14 @@ class Camera(object):
             self.out = cv2.VideoWriter(self.save_video,self.fourcc, self.fps, (self.frame_width,self.frame_height))
         else:
             self.out = None
+
+    def set_roi_dims(self, center, h, w):
+        x = int(center[0]-0.5*w)
+        np.clip(x,0,self.frame_width)
+        y =int(center[1]-0.5*h)
+        np.clip(y,0,self.frame_height)
+
+        self.roi_dims=[x,y,int(h), int(w)]
 
     def capture_frame(self):
         '''
@@ -56,9 +76,29 @@ class Camera(object):
         [x, y, w, h] = self.roi_dims
         self.ret, self.frame = self.cap.read()
         # save cropped frame
-        self.roi = frame[y:y+h, x:x+w]
+        self.roi = self.frame[y:y+h, x:x+w]
 
         return [self.ret, self.frame, self.roi]
+
+    def write_frame(self, frame=None):
+        '''
+        DOC
+        '''
+        if frame is None:
+            frame = self.frame
+        if self.save_video is not None:
+            self.out.write(frame)
+
+    def show_frame(self, frame=None):
+        '''
+        DOC
+        '''
+        if frame is None:
+            frame = self.frame
+        if self.show_video is True:
+            cv2.imshow('frame', frame)
+
+
 
 
     def close(self):
